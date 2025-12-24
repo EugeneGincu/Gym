667a63b7e6fa686523f02d132451ba43820ebdc3
Eugene Gincu <eugene.gincu@gmail.com>
2025-12-23 23:24:42 -0700

    Header menu prototype

--- scripts/headerMenu.js
+++ scripts/headerMenu.js
@@ -3,35 +3,89 @@
 //---------------------------------------------------------------
 "use strict";
 
-let menu = new Map(
+
+//Header Menu
+let links = new Map(
     [
         ['Logo', 'index.html'],
         ['Classes', 'classes.html'],
-        ['Membership', 'membership.html'],
         ['Facility', 'facility.html'],
+        ['Membership', 'membership.html'],
         ['Coaching', 'coaching.html'],
-        ['Locations', 'contact.html'],
-        ['Cross-Reference', 'cross-reference.html']
+        ['Contact', 'contact.html'],
+        ['Cross-Reference', 'cross-reference.html'],
+        ['Locations', 'locations.html'],
+        ['Schedule', 'schedule.html'],
+        ['About', 'about.html'],
+        ['Tools', 'tools.html'],
+        ['Services', 'services.html']
     ]
-);
+)
+// let menu = new Map(
+//     [
+//         // ['Logo', 'index.html'],
+//         ['Classes', 'classes.html'],
+//         ['Membership', 'membership.html'],
+//         ['Facility', 'facility.html'],
+//         ['Coaching', 'coaching.html'],
+//         ['Contact', 'contact.html'],
+//         ['Cross-Reference', 'cross-reference.html']
+//     ]
+// );
 
+let menu = ['Services','Membership','Facility','Coaching','Contact','Cross-Reference'];
 
-let header = document.createElement('header');
+let header = document.querySelector('body > header');
 let nav = document.createElement('nav');
-
 header.appendChild(nav);
-document.getElementsByTagName('body')[0].appendChild(header);
 
-menu.forEach( (value, key) => {
+menu.forEach( value => {
+    let div = document.createElement('div');
     let a = document.createElement('a');
-    a.setAttribute('href', value);
-    a.textContent = key;
-
-    nav.appendChild(a);
+    a.setAttribute('href', links.get(value));
+    // if (key === 'Logo') {
+    //     let img = document.createElement('img');
+    //     img.src = "images/logo.png";
+    //     a.appendChild(img);
+    // }
+    // else
+    a.textContent = value;
+    div.appendChild(a);
+    nav.appendChild(div);
+    a.setAttribute('position', 'relative');
 });
 
 let span = document.createElement('span');
 span.textContent = "Select location";
-nav.appendChild(span);
+header.appendChild(span);
+
+//Header Sub-Menu
+let subMenu = new Map(
+    [
+        ['Contact', ['Location', 'About']],
+        ['Services', ['Classes', 'Schedule', 'Tools']]
+    ]
+)
+
+subMenu.forEach((value, key) => {
+    for (let menu_div of nav.children) {
+        console.log(menu_div.firstChild.textContent);
+        if (menu_div.firstChild.textContent === key) {
+            let submenu_div = document.createElement('div');
+            menu_div.appendChild(submenu_div);
+            menu_div.classList.add('dropdown');
+            for (let submenu_item of value) {
+                let submenu_link = document.createElement('a');
+                submenu_link.textContent = submenu_item;
+                submenu_link.setAttribute('href', links.get(submenu_item));
+                submenu_div.appendChild(submenu_link);
+            }
+            submenu_div.style.position = 'absolute';
+            // submenu_div.style.display = 'none';
+
+        }
+    }
+})
+
 
 
