8664379c515882059ce99960b860a0b848cf203e
Eugene Gincu <eugene.gincu@gmail.com>
2026-01-05 00:10:02 -0700

    Mobile header menu and location selection update

--- scripts/headerMenu.js
+++ scripts/headerMenu.js
@@ -43,12 +43,6 @@ menu.forEach( value => {
     let div = document.createElement('div');
     let a = document.createElement('a');
     a.setAttribute('href', links.get(value));
-    // if (key === 'Logo') {
-    //     let img = document.createElement('img');
-    //     img.src = "images/logo.png";
-    //     a.appendChild(img);
-    // }
-    // else
     a.textContent = value;
     div.appendChild(a);
     if (subMenu.keys().toArray().includes(value))
@@ -57,48 +51,43 @@ menu.forEach( value => {
     // div.setAttribute('position', 'relative');
 });
 
-// document.addEventListener('click', event => {
-//     let submenu = document.querySelector('.mobile_submenu_toggle');
-//     if (submenu && event.target !== submenu) {}
-//         // submenu?.classList.remove('mobile_submenu_toggle');
-// })
-
-
 //Clicking on header menu opens/closes submenu, clicking anywhere in document closes submenu.
 document.addEventListener('click', event => {
     event.preventDefault();
-    // if () return;
-    console.log(event.target.tagName);
 
-    // document.querySelectorAll('')
-
-    // console.log(event.target.parentNode.classList.contains('dropdown')?"true":"false");
+    //Header menu open/close
     if (window.getComputedStyle(nav).flexFlow === "column nowrap" &&
         (event.target.classList.contains('dropdown') || event.target.classList.contains('dropdown_title'))){
         let menu_div = event.target.tagName === "A"? event.target.parentNode : event.target;
         let mobile_submenu = menu_div.querySelector('div.submenu');
-
-        mobile_submenu.classList.toggle('mobile_submenu_show');
-    } else {
-        for (let div of nav.children) {
-            if (div.classList.contains('dropdown')){
-                div.querySelector('div.submenu').classList.remove('mobile_submenu_show');
-            }
+        if (!mobile_submenu.classList.contains('mobile_submenu_show')) {
+            closeMobileDropdowns();
+            mobile_submenu.classList.add('mobile_submenu_show');
         }
-    }
+        else
+            closeMobileDropdowns();
+    } else
+        closeMobileDropdowns();
+
+    //Location submenu open/close
+    if (event.target === location_div.firstChild)
+        location_submenu_div.classList.toggle('location_submenu_show');
+    else
+        location_submenu_div.classList.remove('location_submenu_show');
 });
 
-//Close all mobile header menu dropdowns when window is resized
-window.addEventListener('resize', () => {
+function closeMobileDropdowns() {
     nav.querySelectorAll('div.submenu').forEach( div => {
         div.classList.remove('mobile_submenu_show');
-    })
-});
+    });
+}
+
+//Close all mobile header menu dropdowns when window is resized
+window.addEventListener('resize', () => closeMobileDropdowns());
 
 //Create hidden submenus for each header menu that has a dropdown submenu.
 subMenu.forEach((value, key) => {
     for (let menu_div of nav.children) {
-        // console.log(menu_div.firstChild.textContent);
         if (menu_div.firstChild.textContent === key) {
 
             let submenu_div = document.createElement('div');
@@ -142,20 +131,13 @@ locations.forEach(val => {
     location_submenu_div.appendChild(a);
 })
 
-location_div.firstChild.addEventListener('click', () => {
-    location_submenu_div.classList.toggle('location_submenu_toggle');
-
-})
-
 location_submenu_div.addEventListener('click', event => {
     if (event.target.tagName === 'A') {
-        console.log(event.target.dataset.location);
-        console.log(location_div.querySelector('A').textContent);
         selected_location = event.target.dataset.location;
         location_div.querySelector('A').textContent = event.target.dataset.location;
 
         localStorage.setItem('selectedLocation', selected_location);
-        location_submenu_div.classList.toggle('location_submenu_toggle');
+        location_submenu_div.classList.toggle('location_submenu_show');
 
         //Form location is in sync with the header menu location selection.
         document.getElementById('form_location').value = selected_location;
